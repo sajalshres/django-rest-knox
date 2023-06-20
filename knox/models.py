8be863871c53581a93ac4935831f5c71dfab099a
sajal.shrestha <sajal.shres@gmail.com>
2023-06-20 16:12:19 -0400

    added purpose field

--- knox/models.py
+++ knox/models.py
@@ -14,34 +14,37 @@ User = settings.AUTH_USER_MODEL
 
 class AuthTokenManager(models.Manager):
     def create(
-        self,
-        user,
-        expiry=knox_settings.TOKEN_TTL,
-        prefix=knox_settings.TOKEN_PREFIX
+        self, user, expiry=knox_settings.TOKEN_TTL, prefix=knox_settings.TOKEN_PREFIX
     ):
         token = prefix + crypto.create_token_string()
         digest = crypto.hash_token(token)
         if expiry is not None:
             expiry = timezone.now() + expiry
         instance = super(AuthTokenManager, self).create(
-            token_key=token[:CONSTANTS.TOKEN_KEY_LENGTH], digest=digest,
-            user=user, expiry=expiry)
+            token_key=token[: CONSTANTS.TOKEN_KEY_LENGTH],
+            digest=digest,
+            user=user,
+            expiry=expiry,
+        )
         return instance, token
 
 
 class AbstractAuthToken(models.Model):
-
     objects = AuthTokenManager()
 
-    digest = models.CharField(
-        max_length=CONSTANTS.DIGEST_LENGTH, primary_key=True)
+    digest = models.CharField(max_length=CONSTANTS.DIGEST_LENGTH, primary_key=True)
     token_key = models.CharField(
-        max_length=CONSTANTS.MAXIMUM_TOKEN_PREFIX_LENGTH +
-        CONSTANTS.TOKEN_KEY_LENGTH,
-        db_index=True
+        max_length=CONSTANTS.MAXIMUM_TOKEN_PREFIX_LENGTH + CONSTANTS.TOKEN_KEY_LENGTH,
+        db_index=True,
     )
-    user = models.ForeignKey(User, null=False, blank=False,
-                             related_name='auth_token_set', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User,
+        null=False,
+        blank=False,
+        related_name="auth_token_set",
+        on_delete=models.CASCADE,
+    )
+    purpose = models.CharField(max_length=50, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     expiry = models.DateTimeField(null=True, blank=True)
 
@@ -49,12 +52,12 @@ class AbstractAuthToken(models.Model):
         abstract = True
 
     def __str__(self):
-        return '%s : %s' % (self.digest, self.user)
+        return "%s : %s" % (self.digest, self.user)
 
 
 class AuthToken(AbstractAuthToken):
     class Meta:
-        swappable = 'KNOX_TOKEN_MODEL'
+        swappable = "KNOX_TOKEN_MODEL"
 
 
 def get_token_model():
